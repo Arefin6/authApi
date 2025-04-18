bf15c2b5641f3ba5b1ad0dd86eda40c2304d94be
arefin6 <arefinhossain3@gmail.com>
2025-04-18 20:42:01 +0600

    new variable

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,5 +1,5 @@
 // controllers/paymentController.js
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_Api_KEY);
 const Payment = require("../models/Payment");
 
 exports.checkout = async (req, res) => {
